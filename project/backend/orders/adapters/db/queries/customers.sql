9a1bb7ca9fbcb9443a74a522f81de93ff945eb49
Three Dots Labs <contact@threedotslabs.com>
2026-09-10 09:53:49 +0300

    completed 04-database/02-generate-sqlc

--- project/backend/orders/adapters/db/queries/customers.sql
+++ project/backend/orders/adapters/db/queries/customers.sql
@@ -1,1 +1,11 @@
--- todo: implement
+-- name: InsertCustomer :exec
+INSERT INTO orders.customers (customer_uuid,
+                              name,
+                              email,
+                              address,
+                              phone_number)
+VALUES ($1, $2, $3, $4, $5);
+
+
+-- name: GetCustomerByUUID :one
+SELECT * FROM orders.customers WHERE customer_uuid = $1;
--- project/backend/orders/api/http/handler.go
+++ project/backend/orders/api/http/handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+
 	"eats/backend/common"
 )
 
