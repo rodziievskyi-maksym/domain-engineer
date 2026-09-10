e52072e00ac133b45b830738a4ca5d17ff3cbb45
Three Dots Labs <contact@threedotslabs.com>
2026-09-10 16:23:50 +0300

    completed 05-repository/01-implement-repository

--- project/backend/orders/adapters/db/customer_repo.go
+++ project/backend/orders/adapters/db/customer_repo.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"eats/backend/common/shared"
+	"eats/backend/orders/adapters/db/dbmodels"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 
@@ -24,6 +26,34 @@ func NewCustomerRepository(db *pgxpool.Pool) *CustomerRepository {
 }
 
 func (r *CustomerRepository) RegisterCustomer(ctx context.Context, customerUUID common.UUID, customer http.RegisterCustomer) error {
-	// TODO: implement me
+	address, err := openapiAddressToSharedAddress(customer.Address)
+	if err != nil {
+		return err
+	}
+
+	args := dbmodels.InsertCustomerParams{
+		CustomerUuid: customerUUID,
+		Name:         customer.Name,
+		Email:        string(customer.Email),
+		Address:      address,
+		PhoneNumber:  customer.PhoneNumber,
+	}
+
+	queries := dbmodels.New(r.db)
+
+	if err := queries.InsertCustomer(ctx, args); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func openapiAddressToSharedAddress(addr http.Address) (shared.Address, error) {
+	return shared.NewAddress(
+		addr.Line1,
+		addr.Line2,
+		addr.PostalCode,
+		addr.City,
+		addr.CountryCode,
+	)
 }
