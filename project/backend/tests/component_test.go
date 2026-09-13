65358914f5bcc62694e0457b13b8c2c14e6762bd
Three Dots Labs <contact@threedotslabs.com>
2026-09-13 19:07:32 +0300

    completed 07-errors-and-testing/03-component-tests

--- project/backend/tests/component_test.go
+++ project/backend/tests/component_test.go
@@ -1,11 +1,22 @@
 package tests_test
 
 import (
+	"eats/backend/common/testutils"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestComponent_CriticalFlow(t *testing.T) {
 	t.Parallel()
 
-	t.Error("TODO - implement me!")
+	clients := newTestClients(t)
+
+	ctx := t.Context()
+
+	randomCountry := testutils.GenerateRandomCountry()
+
+	customerUUID := registerCustomerInCity(ctx, t, clients, randomCountry, "Some city")
+
+	assert.NotEmpty(t, customerUUID)
 }
